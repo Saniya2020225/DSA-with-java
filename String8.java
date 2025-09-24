60991da9ae0b31fa98e1ce2337953f4b1d3a4042
saniya kushwaha <saniyakushwaha59@gmail.com>
2025-09-24 23:23:54 +0530

    StringBuilder

---
+++ String8.java
@@ -0,0 +1,24 @@
+public class String8 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Tony");
+        System.out.println(sb);
+
+        //chat at index
+        System.out.println(sb.charAt(3));
+
+        //set char at index
+        sb.setCharAt(0 , 'p');
+        System.out.println(sb);
+
+        //insert index
+        sb.insert(2, 'n');
+        System.out.println(sb);
+
+        //delete the extra 'n'
+        sb.delete(2, 3);
+        System.out.println(sb);
+        sb.delete(2, 3);
+        System.out.println(sb);
+        
+    }
+}
---
+++ String9.java
@@ -0,0 +1,11 @@
+public class String9 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("h");
+        sb.append("e"); //str = str + "e";
+        sb.append("l"); //str = str + "l";
+        sb.append("l"); //str = str + "l";
+        sb.append("o"); //str = str + "o";
+        System.out.println(sb);
+        System.out.println(sb.length());
+    }
+}
